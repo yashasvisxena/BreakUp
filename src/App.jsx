32b529e6dc134916a9fa11cc5c5a9fd8a181d3cd
yashasvisxena <yashasvi1802@gmail.com>
2023-12-07 01:23:03 +0530

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./components/common/Navbar";
+import Main from "./components/main/Main";
+
+function App() {
+  return (
+    <div className="h-full w-screen absolute">
+      <Navbar />
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/main/Main.jsx
@@ -0,0 +1,15 @@
+// import Dashboard from "./Dashboard"
+
+import Specificgrp from "./Specificgrp"
+
+
+const Main = () => {
+  return (
+    <div className="h-screen mx-2 mb-2 p-2">
+        {/* <Dashboard/> */}
+        <Specificgrp/>
+    </div>
+  )
+}
+
+export default Main
